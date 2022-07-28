10d909b9ca900ee7b313d161919da17306ad8886
Dmitry Dzhigaev <ddzhigaev@gmail.com>
2022-07-28 15:13:06 +0200

    Create __init__.py

---
+++ sxdm/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+# sxdm: tools for scanning x-ray diffraction mapping
+#   (c) 2022 : Lund University
+#       authors:
+#         Dmitry Dzhigaev, ddzhigaev@gmail.com
+"""The main sxdm package, which contains the whole framework."""
+__version__ = "0.0.1"
